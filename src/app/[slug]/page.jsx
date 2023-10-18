9dc6ddef6eeef5b0e20cd1aafe4919c16b3e2728
PRIZEOX <oganisyanararat1@gmail.com>
2023-10-18 20:57:00 +0400

    some new components

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './singlePage.module.css'
+import { Menu } from '@/components/menu/Menu'
+import Image from 'next/image'
+
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.infoContainer}>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Азер - необязательно человек</h1>
+                <div className={styles.user}>
+                    <div className={styles.userImageContainer}>
+                        <Image alt='' src='/p1.jpeg' fill className={styles.avatar}/>
+                    </div>
+                    <div className={styles.userTextContainer}>
+                        <span className={styles.username}>Nikol Pashaev</span>
+                        <span className={styles.date}>01.02.2023</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image alt='' src='/p1.jpeg' fill className={styles.image}/>
+            </div>
+        </div>
+        <div className={styles.content}>
+            <div className={styles.post}>
+                <div className={styles.desc}>
+                    <p> Для современного мира социально-экономическое развитие,
+                        а также свежий взгляд на привычные вещи — безусловно открывает новые горизонты для кластеризации усилий.
+                        А ещё предприниматели в сети интернет рассмотрены исключительно в разрезе маркетинговых и финансовых предпосылок.
+                    </p>
+                </div>
+            </div>
+            <Menu/>
+        </div>
+    </div>
+  )
+}
+
+export default SinglePage
